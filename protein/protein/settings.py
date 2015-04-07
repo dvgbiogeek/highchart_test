64f32b58f71ac5c906614e5b6115fffd27a7e3dc
glickdv <dvgbiogeek@gmail.com>
2015-04-06 22:57:37 -0700

    test heroku deployment

--- protein/protein/settings.py
+++ protein/protein/settings.py
@@ -143,3 +143,25 @@ TASTYPIE_DEFAULT_FORMATS = ['json']
 TASTYPIE_FULL_DEBUG = True
 
 LOGIN_URL = '/login/'
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+DATABASES['default']['ENGINE'] = 'django_postgrespool'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = 'staticfiles'
+# STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+
+
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
