c0d4a28921812c50e84ee6565119bc2a8dd0d046
glickdv <dvgbiogeek@gmail.com>
2015-04-09 23:56:34 -0700

    updates to static file path

--- protein/settings/__init__.py
+++ protein/settings/__init__.py
@@ -85,11 +85,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '../static/'
 
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '/static/'))
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static/'))
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), 'static',)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, '../static'), 'static',)
 
 LOGGING = {
     'version': 1,
