2b66107c94f216bda1f166accff006e16d3c720c
glickdv <dvgbiogeek@gmail.com>
2015-04-10 00:06:41 -0700

    try again

--- protein/settings/__init__.py
+++ protein/settings/__init__.py
@@ -159,7 +159,7 @@ DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '../static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '../static'),
